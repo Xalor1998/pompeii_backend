e0f495051cb627c7ce53b644792c7e88d98291ea
Xalor <xalor1998@gmail.com>
2024-06-08 21:12:01 +0200

    initial commit

---
+++ src/main/java/com/cex/vulcano/PompeiiApplication.java
@@ -0,0 +1,13 @@
+package com.cex.vulcano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PompeiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PompeiiApplication.class, args);
+	}
+
+}
